b93e3a08af76384e998b2bc34f592cb64a85ac39
Germán Martinez <germand_m@hotmail.com>
2022-05-26 15:57:06 -0300

    Completo el crud de Familia

--- familia/views.py
+++ familia/views.py
@@ -1,7 +1,9 @@
+from readline import append_history_file
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponseBadRequest
 from django.template import loader
-from django.shortcuts import render
-from familia.forms import PersonaForm, BuscarPersonasForm
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from familia.forms import PersonaForm, BuscarPersonasForm, ActualizarPersonaForm
 
 from familia.models import Persona
 
@@ -31,7 +33,7 @@ def agregar(request):
             altura = form.cleaned_data['altura']
             Persona(nombre=nombre, apellido=apellido, email=email, fecha_nacimiento=fecha_nacimiento, altura=altura).save()
 
-            return HttpResponseRedirect("/")
+            return HttpResponseRedirect(reverse("index"))
     elif request.method == "GET":
         form = PersonaForm()
     else:
@@ -50,28 +52,53 @@ def borrar(request, identificador):
         persona = Persona.objects.filter(id=int(identificador)).first()
         if persona:
             persona.delete()
-        return HttpResponseRedirect("/familia/")
+        return HttpResponseRedirect(reverse("index"))
     else:
         return HttpResponseBadRequest("Error no conzco ese metodo para esta request")
 
 
-def actualizar(request, identificador):
+def actualizar(request, identificador=''):
     '''
     TODO: implementar una vista para actualización
     '''
-    pass
+    if request.method == "GET":
+        persona = get_object_or_404(Persona, pk=int(identificador))
+        initial = {
+            "id": persona.id,
+            "nombre": persona.nombre, 
+            "apellido": persona.apellido, 
+            "email": persona.email,
+            "fecha_nacimiento": persona.fecha_nacimiento.strftime("%d/%m/%Y"),
+            "altura": persona.altura,
+        }
+    
+        form_actualizar = ActualizarPersonaForm(initial=initial)
+        return render(request, 'familia/form_carga.html', {'form': form_actualizar, 'actualizar': True})
+    
+    elif request.method == "POST":
+        form_actualizar = ActualizarPersonaForm(request.POST)
+        if form_actualizar.is_valid():
+            persona = get_object_or_404(Persona, pk=form_actualizar.cleaned_data['id'])
+            persona.nombre = form_actualizar.cleaned_data['nombre']
+            persona.apellido = form_actualizar.cleaned_data['apellido']
+            persona.email = form_actualizar.cleaned_data['email']
+            persona.fecha_nacimiento = form_actualizar.cleaned_data['fecha_nacimiento']
+            persona.altura = form_actualizar.cleaned_data['altura']
+            persona.save()
+
+            return HttpResponseRedirect(reverse("index"))
 
 
 def buscar(request):
-    if request.method == "GET":
+    
+    if request.GET.get("palabra_a_buscar") and request.method == "GET":
+        form_busqueda = BuscarPersonasForm(request.GET)
+        if form_busqueda.is_valid():
+            personas = Persona.objects.filter(nombre__icontains=request.GET.get("palabra_a_buscar"))
+            return  render(request, 'familia/lista_familiares.html', {"personas": personas, "resultados_busqueda":True})
+
+    elif request.method == "GET":
         form_busqueda = BuscarPersonasForm()
         return render(request, 'familia/form_busqueda.html', {"form_busqueda": form_busqueda})
-
-    elif request.method == "POST":
-        form_busqueda = BuscarPersonasForm(request.POST)
-        if form_busqueda.is_valid():
-            palabra_a_buscar = form_busqueda.cleaned_data['palabra_a_buscar']
-            personas = Persona.objects.filter(nombre__icontains=palabra_a_buscar)
-
-        return  render(request, 'familia/lista_familiares.html', {"personas": personas})
+        
     
